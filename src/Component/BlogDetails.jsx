5cb6eb8ac29daf53ed059125fffeb2df0aecc2c9
Aisha Muhammd Aliyu <[ashmah017@gmail.com]>
2025-02-15 14:27:58 +0100

    added speech-Synthesis

--- src/Component/BlogDetails.jsx
+++ src/Component/BlogDetails.jsx
@@ -1,15 +1,113 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import angleleft from '../assets/angle-left.svg';
+import menu from '../assets/content.svg';
+import Aa from '../assets/Aa.svg';
+import voice from '../assets/voice.svg';
+import bookmarks from '../assets/bookmark.svg';
+import fillBookmark from '../assets/bookmark-fill.svg';
+import pausy from '../assets/pause.svg';
+import blog from './Blogs.json';
 
 const Blog = () => { 
-    const {id} = useParams()
+  const {id} = useParams();
+  const blogDetails = blog.find(blog => blog.id === id);
+
+  if (!blogDetails) {
+    return <div>Blog not found</div>;
+  }
+
+  const navigate = useNavigate();
+  const back = () => {
+    navigate(-1);
+  };
+
+  const [Bookmark, setBookmark] = useState(false);
+  const handleBookmark = () => {
+    setBookmark(!Bookmark);
+  };
+
+  const [Speaking, setSpeaking] = useState(false);
+  const [Pause, setPause] = useState(false);
+  const [instance, setinstance] = useState(null);
+  const texttoSpeech  = window.speechSynthesis
+
+  const text = 'aysha';
+
+  const voiceRead = () => {
+    if('speechSynthesis' in window){
+     if(Pause){
+      texttoSpeech.resume();
+      setPause(false)
+    
+    }else{
+    texttoSpeech.cancel();
+    const speech = new SpeechSynthesisUtterance(blogDetails.summary);
+    speech.lang ='en-Us'
+    speech.rate = 1;
+    speech.pitch = 5
+
+
+    speech.onstart =(()=>setSpeaking(true));
+    speech.onend = ()=>{
+      setPause(false);
+      setSpeaking(false)
+    }
+    texttoSpeech.speak(speech);
+    setinstance(speech)
+    
+  }
+}}
+ 
+ const handlepause=()=>{
+  if(texttoSpeech.speaking && !Pause ){
+    texttoSpeech.pause()
+    setPause(true)
+
+  }else if (texttoSpeech.paused){
+    setPause(false)
+    texttoSpeech.resume()
+  }
+ 
+ }
 
   return (
     <div>
-      <h1>blog detaild - {id}</h1>
-    </div>
-  )
-}
+      <div className='flex justify-between shadow p-4'>
+        <img src={angleleft} onClick={back} className="w-10 cursor-pointer" />
+        <img src={menu} className="w-[2rem] cursor-pointer" />
+        <img src={Aa} className="w-[2rem] cursor-pointer" />
+        {<button onClick={Speaking ? handlepause : voiceRead}>       
+          <img src={Pause ? pausy: voice  } className="w-10 cursor-pointer" />
+        </button>}
+       
+       <button onClick={handleBookmark}>
+          {Bookmark ? 
+            <img src={fillBookmark} className="w-[2rem] cursor-pointer" /> : 
+            <img src={bookmarks} className="w-[2rem] cursor-pointer" />
+          }
+        </button>
+      </div>
 
-export default Blog
- 
+      <div className="p-10 overflow-scroll mb-6">
+        <h1 className='text-2xl font-primaryBold text-gray-600 mb-8'>{blogDetails.title}</h1>
+        <h1 className='m-3 font-primaryRegular text-gray-600'>{blogDetails.summary}</h1>
+        <h1 className='text-gray-600 m-3'>
+          <span className="font-primaryBold">Lesson: </span>
+          <span className="font-primaryRegular">{blogDetails.lesson}</span>
+        </h1>
+        <h1 className='text-gray-600 m-3'>
+          <span className='font-primaryBold'>Reference: </span>
+          <span className='font-primaryRegular'>{blogDetails.reference}</span>
+        </h1>
+      </div>
+
+      <div className="bg-white fixed bottom-0 border shadow-2xl p-4 flex justify-between w-full">
+        <h1>Page 1 out of 1</h1>
+        <h1>Page 1 out of 1</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
--- src/Component/Blogs.jsx
+++ src/Component/Blogs.jsx
@@ -7,7 +7,7 @@ const Blogs = () => {
   return (
     <div className="">
     <h1 className='ml-5 m-2 text-2xl text-gray-600 '>Blogs</h1>
-    <div className='grid grid-cols-2 md:grid-cols-3 gap-6 p-6'>
+    <div className='grid  grid-cols-2  md:grid-cols-3 gap-6 p-6'>
       {
         blogs.map((records, index)=>{
           const spliceText = records.summary.slice(0,150)
@@ -19,8 +19,9 @@ const Blogs = () => {
           <p className='text-[.9rem] p-2 text-gray-600'>{spliceText}...... </p>
             <h3 className='text-gray-600 font-semi'> <span className='font-semibold'>Lesson:</span> <span className='text-[.9rem]'> {records.lesson}</span> </h3>
             <h3 className='text-gray-600'><span className='font-semibold'>Reference</span> <span className='text-[.9rem]'> {records.reference}</span> </h3>
-            <Link to={`/BlogsDetails/${records.title}`}>
-                <button className="bg-gray-200 border border-gray-300 w-[17%] mt-3 cursor-pointer hover:text-gray-200 hover:bg-gray-500 transition-all duration-300 rounded p-[.2rem] text-gray-600 text-[.8rem]">Read more</button>
+            <img src={records.img}className='w-20 mt-4 mb-2 rounded' />
+            <Link to={`/BlogsDetails/${records.id}`}>
+                <button className="bg-gray-200 border border-gray-300 mt-3 cursor-pointer hover:text-gray-200 hover:bg-gray-500 transition-all duration-300 rounded p-[.2rem] text-gray-600 text-[.8rem]">Read more</button>
 
             </Link>
 
